c0041f3a05fb42ff8b227bfbe97ada81f1c7998e
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-12 21:33:40 -0500

    gave editor its own MonoChain

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -49,5 +49,7 @@ private:
 
     std::vector<juce::Component*> getComps();
 
+    MonoChain monoChain;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (JulesEQAudioProcessorEditor)
 };
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -27,6 +27,17 @@ struct ChainSettings
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
 
+using Filter = juce::dsp::IIR::Filter<float>;
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
+
 //==============================================================================
 /**
 */
@@ -75,18 +86,11 @@ public:
 
 private:
 
-    using Filter = juce::dsp::IIR::Filter<float>;
-    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
 
     MonoChain leftChain, rightChain;
 
-    enum ChainPositions
-    {
-        LowCut,
-        Peak,
-        HighCut
-    };
+
 
     void updatePeakFilter(const ChainSettings& chainSettings);
     using Coefficients = Filter::CoefficientsPtr;
